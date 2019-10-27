5d8824862ee6c6e4d61fbde26a32eb1a7496b08b
mih9783 <mih9783@yandex.ru>
2019-10-27 07:57:37 +0700

    7 урок  touch

---
+++ 7-EVENTS-TOUCH/js/functions.js
@@ -0,0 +1,21 @@
+
+    // Touch события
+
+    /*
+    касание (или клик на компе)
+    */
+
+    // $(document).on("pageinit", "#pageone", function () {
+    //     $(".para_touch").on("tap", function () {
+    //         $(this).hide();
+    //     });
+    // });
+
+
+
+
+$(document).on("pageinit", "#pageone", function () {
+    $(".para_touch").on("tap", function () {
+        $(this).hide();
+    });
+});
